12e3ac6dd4a190c1e91681dbb998f52faefa4106
Suzaalx <sujalxetry00@gmail.com>
2023-05-04 22:55:26 +0545

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Bruno: ["Bruno Ace SC", "cursive"],
+
+      },
+    },
+  },
+  plugins: [],
+}
